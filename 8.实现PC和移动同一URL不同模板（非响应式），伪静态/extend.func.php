34f7acd8ac9c2244651975938a8fcb2a4e8e25e6
weiyuanchuang <luobo157@126.com>
2020-11-18 16:49:08 +0800

    Add files via upload

---
+++ 8.实现PC和移动同一URL不同模板（非响应式），伪静态/extend.func.php
@@ -0,0 +1,47 @@
+<?php
+function litimgurls($imgid=0)
+{
+    global $lit_imglist,$dsql;
+    //获取附加表
+    $row = $dsql->GetOne("SELECT c.addtable FROM #@__archives AS a LEFT JOIN #@__channeltype AS c 
+                                                            ON a.channel=c.id where a.id='$imgid'");
+    $addtable = trim($row['addtable']);
+    
+    //获取图片附加表imgurls字段内容进行处理
+    $row = $dsql->GetOne("Select imgurls From `$addtable` where aid='$imgid'");
+    
+    //调用inc_channel_unit.php中ChannelUnit类
+    $ChannelUnit = new ChannelUnit(2,$imgid);
+    
+    //调用ChannelUnit类中GetlitImgLinks方法处理缩略图
+    $lit_imglist = $ChannelUnit->GetlitImgLinks($row['imgurls']);
+    
+    //返回结果
+    return $lit_imglist;
+}
+
+/**
+ * 判断是否为移动设备访问
+ * @return bool
+ */
+function isMobile()
+{
+    if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
+        return true;
+    }
+    if (isset($_SERVER['HTTP_VIA'])) {
+        return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+    }
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+        $clientkeywords = array('nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp', 'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu', 'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi', 'openwave', 'nexusone', 'cldc', 'midp', 'wap', 'mobile');
+        if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+            return true;
+        }
+    }
+    if (isset($_SERVER['HTTP_ACCEPT'])) {
+        if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'textml') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'textml')))) {
+            return true;
+        }
+    }
+    return false;
+}
